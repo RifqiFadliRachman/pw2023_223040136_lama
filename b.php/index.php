49cf2f3af3f4a771f202e8203ee6b50e325766c2
RifqiFadliRachman <124899367+RifqiFadliRachman@users.noreply.github.com>
2023-02-23 11:30:06 +0700

    Add files via upload

---
+++ b.php/index.php
@@ -0,0 +1,22 @@
+<?php 
+$npm ="36";
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas1</title>
+ </head>
+ <body>
+    <p>
+        Aku adalah angka <b><?php echo $npm ?></b><br>
+        Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $npm * 5 ?></b><br>
+        Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $npm / 2 ?></b><br>
+        jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $npm + 75 ?></b><br>
+        Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $npm - 20 ?></b><br>
+
+    </p>
+ </body>
+ </html>
